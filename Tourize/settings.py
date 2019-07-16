e7d9efcf8b547136ebaa37d9d4c78f117cd75d18
Omar-Abdel-Wahab <omar471198@yahoo.com>
2019-07-16 23:41:12 +0200

    Cors problem:Allow all

--- Tourize/settings.py
+++ Tourize/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,7 +51,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'Tourize.urls'
@@ -83,6 +83,7 @@ CORS_ORIGIN_WHITELIST =(
     'https://localhost:8100',
 )
 
+CORS_ORIGIN_ALLOW_ALL = True
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
